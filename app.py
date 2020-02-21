63f95e9159a0966da0e59279f8fe39a30e726509
cawthornebr <56853166+cawthornebr@users.noreply.github.com>
2020-02-20 20:28:20 -0700

    commented app.py

--- app.py
+++ app.py
@@ -38,75 +38,131 @@ def index():
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     print("---Precipitation selected.---")
+    #query
     dates = session.query(Measurement).order_by(Measurement.date.desc())
+
+    #close session
     session.close()
+
+    #set count and lists
     count = 0
     year_dates = []
     year_prcp = []
+
+    #loop through query
     for date in dates:
+
+        #pull date from query and format it
         date_new = date.date
         date_new = datetime.strptime(date_new, "%Y-%m-%d")
+
         if count==0:
+
+            #for first date, set end date
             end_date = date_new - relativedelta(months=+12)
             count+=1
+
         if date_new >= end_date:
             if(date.prcp != None):
+
+                #add precipitation and date to lists
                 year_prcp.append(date.prcp)
                 year_dates.append(date.date)
+    
+    #set dictionary for dates and precipitation
     pdict = {"Date":year_dates, "Precipitation": year_prcp}
+
     return jsonify(pdict)
 
 @app.route("/api/v1.0/stations")
 def stations():
     print("---Stations selected.---")
+
+    #query
     stations_data = session.query(Station.name).order_by(Station.name.desc()).all()
-    stations_data = [stations_data[0] for stations_data in stations_data]
+
+    #close session
     session.close()
+
+    #loop through query and set new list
+    stations_data = [stations_data[0] for stations_data in stations_data]
+
     return jsonify(stations_data)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
     print("---Temperature (tobs) selected.---")
+
+    #==========Get most recent date and format it==========
+    #query
     temp_query_date = session.query(Measurement.date).\
         order_by(Measurement.date.desc()).all()
+    
+    #loop through query and set new list
     temp_query_date = [temp_query_date[0] for temp_query_date in temp_query_date]
+
+    #format
     temp_query_dates = datetime.strptime(temp_query_date[0], "%Y-%m-%d")
-    #==========Get most recent date and format it======
+    #==========Get most recent date and format it==========
 
     #query
     temp_query = session.query(Measurement.tobs).\
         filter(Measurement.date >= (temp_query_dates - dt.timedelta(days=365))).all()
-    temp_query = [temp_query[0] for temp_query in temp_query]
+    
+    #close session
     session.close()
+
+    #loop through query and set new list
+    temp_query = [temp_query[0] for temp_query in temp_query]
+
     return jsonify(temp_query)
 
 @app.route("/api/v1.0/<start>")
 def start_date(start):
     print("---Start date selected.---")
     try:
+
+        #query
         dates = session.query(Measurement.date).all()
+
+        #loop through query and set new list
         dates = [dates[0] for dates in dates]
+
         if start in dates:
+
+                #query
                 start_date_query = session.query(Measurement.tobs).\
                     filter(Measurement.date >= (start)).all()
-                start_date_query = [start_date_query[0] for start_date_query in start_date_query]
+
+                #close session
                 session.close()
+                
+                #loop through query and set new list
+                start_date_query = [start_date_query[0] for start_date_query in start_date_query]
+
+                #find average
                 average = round(sum(start_date_query) / len(start_date_query),1)
+
+
                 return jsonify(f"Minimum : {min(start_date_query)}",
                     f"Average : {average}",
                     f"Maximum : {max(start_date_query)}")
         else:
+
+            #close session
             session.close()
-            return jsonify({"1error": f"This date, {start}, is not in the correct format or is outside the scope of this database. Please enter a valid date in the correct format: Year-Month-Day."}), 404
+            return jsonify({"error": f"This date, {start}, is not in the correct format or is outside the scope of this database. Please enter a valid date in the correct format: Year-Month-Day."}), 404
     except:
+
+        #close session
         session.close()
-        return jsonify({"2error": f"This date, {start}, is not in the correct format. Please enter a valid date in the correct format: Year-Month-Day."}), 404
+        return jsonify({"error": f"This date, {start}, is not in the correct format. Please enter a valid date in the correct format: Year-Month-Day."}), 404
     
 @app.route("/api/v1.0/<start>/<end>")
 def start_and_end_date(start,end):
     print("---Start and end date selected.---")
     
-    ###### DATE FORMAT TEST
+    #==========DATE FORMAT TEST==========
 
     #test to make sure the start date is in the correct date format
     start_test = False
@@ -126,21 +182,31 @@ def start_and_end_date(start,end):
     except:
         return jsonify({"error": f"This END date, {end}, is not in the correct format. Please enter a valid date in the correct format: Year-Month-Day."}), 404
 
-    ######## DATE FORMAT TEST
+    #==========DATE FORMAT TEST==========
     
     start1 = str(start.date())
     end1 = str(end.date())
 
     if end_test and start_test == True:
+
         if end > start:
+
             dates = session.query(Measurement.date).all()
             dates = [dates[0] for dates in dates]
+
             if start1 and end1 in dates:
+
                     start_date_query = session.query(Measurement.tobs).\
                         filter(Measurement.date >= (start), Measurement.date <= (end)).all()
+
                     start_date_query = [start_date_query[0] for start_date_query in start_date_query]
+                    
+                    #close session
                     session.close()
+
+                    #average
                     average = round(sum(start_date_query) / len(start_date_query),1)
+
                     return jsonify(f"Minimum : {min(start_date_query)}",
                         f"Average : {average}",
                         f"Maximum : {max(start_date_query)}")
